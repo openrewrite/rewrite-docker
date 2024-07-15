8374ab2e9cbac056160ef5fddde0ba66990178b5
Jonathan Schneider <jkschneider@gmail.com>
2024-07-14 23:12:00 -0400

    Simplify build

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,28 +10,3 @@ dependencies {
     implementation(platform("org.openrewrite.recipe:rewrite-recipe-bom:$rewriteVersion"))
     implementation("org.openrewrite:rewrite-core:$rewriteVersion")
 }
-
-signing {
-    isRequired = false
-}
-
-configure<nebula.plugin.release.git.base.ReleasePluginExtension> {
-    defaultVersionStrategy = nebula.plugin.release.NetflixOssStrategies.SNAPSHOT(project)
-}
-
-configure<PublishingExtension> {
-    publications {
-        named("nebula", MavenPublication::class.java) {
-            suppressPomMetadataWarningsFor("runtimeElements")
-        }
-    }
-}
-
-publishing {
-    repositories {
-        maven {
-            name = "moderne"
-            url = uri("https://us-west1-maven.pkg.dev/moderne-dev/moderne-recipe")
-        }
-    }
-}
